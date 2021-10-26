dffc14bc7a35693b84d2873905569835ffd0b203
anup-stha <xxanup.sthaxx@gmail.com>
2021-10-26 19:47:59 +0545

    Made Login Screen Responsive

--- src/modules/auth/LoginPage.tsx
+++ src/modules/auth/LoginPage.tsx
@@ -50,9 +50,9 @@ export const LoginPage = () => {
   };
 
   return (
-    <div className="h-screen py-20 overflow-hidden px-72 lg:bg-red-700 bg-green-50 lg:px-2">
-      <div className="flex h-full bg-white rounded-3xl shadow-E400">
-        <div className="flex flex-col items-center justify-center w-2/5 bg-green-400 rounded-tr-none rounded-br-none gap-y-24 z-2 bg-transition rounded-3xl ">
+    <div className="flex items-center justify-center w-full h-screen py-20 overflow-hidden bg-green-50 lg:py-64 md:px-8 sm:p-0 ">
+      <div className="flex w-full h-full bg-white max-w-6xl rounded-3xl shadow-E400 3xl:max-w-[80%] sm:max-w-none">
+        <div className="flex flex-col items-center justify-center w-2/5 bg-green-400 rounded-tr-none rounded-br-none sm:rounded-none sm:w-0 gap-y-24 z-2 bg-transition rounded-3xl sm:opacity-0 ">
           <Image
             src={LoginAvatar}
             alt="Login Avatar"
@@ -74,57 +74,60 @@ export const LoginPage = () => {
             className="filter drop-shadow-2xl text-gray-850"
           />
         </div>
-        <div className="flex flex-col justify-between w-3/5 px-24 bg-white rounded-tl-none rounded-bl-none py-28 rounded-3xl bg-out-transition fadeInLogin">
-          <div className="flex flex-col text-left">
-            <p className="text-5xl font-medium text-gray-850">
-              Hello! Welcome Back
-            </p>
-            <p className=" text-[1.4rem] font-light text-gray-850 ">
-              Log in with your credentials you entered during your registration.
+        <div className="flex items-center w-3/5 px-20 py-32 rounded-tl-none rounded-bl-none sm:px-8 sm:bg-green-50 sm:rounded-none sm:w-full sm:h-full sm:py-6 lg:px-12 rounded-3xl bg-out-transition fadeInLogin ">
+          <div className="flex flex-col justify-between h-full bg-white sm:h-auto lg:justify-center sm:px-8 sm:shadow-E400 sm:py-8 lg:gap-y-16 sm:gap-y-12 sm:rounded-xl">
+            <div className="flex flex-col text-left">
+              <p className="text-5xl font-medium text-gray-800 sm:text-4xl">
+                Hello! Welcome Back
+              </p>
+              <p className=" text-[1.4rem] font-light text-gray-850 lg:text-lg ">
+                Log in with your credentials you entered during your
+                registration.
+              </p>
+            </div>
+
+            <form className="w-full space-y-8" onSubmit={(e) => onLogin(e)}>
+              <div className="space-y-6 ">
+                <PrimaryInput
+                  type="email"
+                  id="Email"
+                  value={email}
+                  onChange={(e: any) => setEmail(e.target.value)}
+                />
+                <PrimaryInput
+                  type="password"
+                  id="Password"
+                  value={password}
+                  onChange={(e: any) => setPassword(e.target.value)}
+                />
+              </div>
+              <Button type="loading" loading={loading}>
+                Login
+              </Button>
+            </form>
+
+            <p className="text-base font-medium text-gray-600">
+              * By logging in you accept our
+              <a
+                className="text-green-500 cursor-pointer"
+                href="https://sunyahealth-landing.vercel.app/privacy"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Privacy Policy
+              </a>{" "}
+              and{" "}
+              <a
+                className="text-green-500 cursor-pointer"
+                href="https://sunyahealth-landing.vercel.app/terms"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Terms of Service
+              </a>
+              .
             </p>
           </div>
-
-          <form className="w-full space-y-8" onSubmit={(e) => onLogin(e)}>
-            <div className="space-y-6 ">
-              <PrimaryInput
-                type="email"
-                id="Email"
-                value={email}
-                onChange={(e: any) => setEmail(e.target.value)}
-              />
-              <PrimaryInput
-                type="password"
-                id="Password"
-                value={password}
-                onChange={(e: any) => setPassword(e.target.value)}
-              />
-            </div>
-            <Button type="loading" loading={loading}>
-              Login
-            </Button>
-          </form>
-
-          <p className="text-base font-medium text-gray-600">
-            * By logging in you accept our
-            <a
-              className="text-green-500 cursor-pointer"
-              href="https://sunyahealth-landing.vercel.app/privacy"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Privacy Policy
-            </a>{" "}
-            and
-            <a
-              className="text-green-500 cursor-pointer"
-              href="https://sunyahealth-landing.vercel.app/terms"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Terms of Service
-            </a>
-            .
-          </p>
         </div>
       </div>
     </div>
