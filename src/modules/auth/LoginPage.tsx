2aee7d2b9ee412b0f9115569988ee00bd70bebe2
anup-stha <anupstha012@gmail.com>
2021-12-31 23:49:11 +0545

    Fixed Bugs of Login Page

--- src/modules/auth/LoginPage.tsx
+++ src/modules/auth/LoginPage.tsx
@@ -1,7 +1,7 @@
 /*
  * Created By Anup Shrestha
  * Copyright (c) 2021. All rights reserved.
- * Last Modified 12/31/21, 1:08 PM
+ * Last Modified 12/31/21, 11:48 PM
  *
  *
  */
@@ -16,7 +16,7 @@ export const LoginPage = () => {
   return (
     <div
       suppressHydrationWarning={true}
-      className="flex flex-col items-center justify-center w-full min-h-screen py-20 bg-gray-50 sm:bg-gray-50  lg:py-8 md:px-8 sm:p-0 sm:h-screen"
+      className="flex flex-col items-center justify-center w-full min-h-screen py-20 bg-gray-100 sm:bg-gray-200  lg:py-8 md:px-8 sm:p-0 sm:h-screen"
     >
       <div className="flex w-full bg-white max-w-6xl flex-grow rounded-3xl shadow-E400 3xl:max-w-[60%] sm:max-w-none h-full">
         <div className="flex flex-col items-center justify-center w-2/5 bg-green-400 rounded-tr-none rounded-br-none sm:rounded-none sm:w-0 gap-y-24 z-2 bg-transition rounded-3xl sm:opacity-0 ">
@@ -41,39 +41,42 @@ export const LoginPage = () => {
             className="filter drop-shadow-2xl text-gray-850"
           />
         </div>
-        <div className="flex flex-col justify-between gap-y-20 w-3/5 px-20 py-32 rounded-tl-none rounded-bl-none sm:px-8 sm:bg-white sm:rounded-none sm:justify-center sm:w-full sm:h-full sm:py-6 lg:px-12 rounded-3xl bg-out-transition fadeInLogin ">
-          <div className="flex flex-col text-left">
-            <p className="text-5xl font-medium text-gray-800 sm:text-4xl lg:text-3xl">
-              Hello! Welcome Back
-            </p>
-            <p className=" text-[1.4rem] font-light text-gray-850 lg:text-lg ">
-              Log in with your credentials you entered during your registration.
+        <div className="flex flex-col justify-between gap-y-20 w-3/5 px-20 py-32 sm:px-4 sm:bg-gray-200 rounded-tl-none rounded-bl-none  sm:rounded-none sm:justify-center sm:w-full sm:h-full sm:py-6 lg:px-12  bg-out-transition fadeInLogin ">
+          <div className="gap-y-20 flex flex-col sm:px-8 sm:py-12 sm:rounded-2xl sm:shadow-E300 sm:bg-white">
+            <div className="flex flex-col text-left">
+              <p className="text-5xl font-medium text-gray-800 sm:text-4xl lg:text-3xl">
+                Hello! Welcome Back
+              </p>
+              <p className=" text-[1.4rem] font-light text-gray-850 lg:text-lg ">
+                Log in with your credentials you entered during your
+                registration.
+              </p>
+            </div>
+            <LoginForm />
+
+            <p className="text-base font-medium text-gray-600">
+              * By logging in you accept our
+              <a
+                className="text-green-500 cursor-pointer hover:text-green-600"
+                href="https://sunyahealth-landing.vercel.app/privacy"
+                target="_blank"
+                rel="noreferrer"
+              >
+                {" "}
+                Privacy Policy
+              </a>{" "}
+              and{" "}
+              <a
+                className="text-green-500 cursor-pointer hover:text-green-600"
+                href="https://sunyahealth-landing.vercel.app/terms"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Terms of Service
+              </a>
+              .
             </p>
           </div>
-          <LoginForm />
-
-          <p className="text-base font-medium text-gray-600">
-            * By logging in you accept our
-            <a
-              className="text-green-500 cursor-pointer hover:text-green-600"
-              href="https://sunyahealth-landing.vercel.app/privacy"
-              target="_blank"
-              rel="noreferrer"
-            >
-              {" "}
-              Privacy Policy
-            </a>{" "}
-            and{" "}
-            <a
-              className="text-green-500 cursor-pointer hover:text-green-600"
-              href="https://sunyahealth-landing.vercel.app/terms"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Terms of Service
-            </a>
-            .
-          </p>
         </div>
       </div>
     </div>
