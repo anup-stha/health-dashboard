be4cc60b9ce8b995480df5fc50c5ceb1236ecf53
anup-stha <anup.stha012@gmail.com>
2022-01-03 11:14:14 +0545

    Changes

--- src/modules/members/table/memberTableRow.tsx
+++ src/modules/members/table/memberTableRow.tsx
@@ -1,7 +1,7 @@
 /*
  * Created By Anup Shrestha
  * Copyright (c) 2021-2022. All rights reserved.
- * Last Modified 1/2/22, 6:10 PM
+ * Last Modified 1/3/22, 11:13 AM
  *
  *
  */
@@ -98,7 +98,7 @@ export const MemberTableRow: React.FC<OrgTableRowType> = ({ data, key, loading }
         </td>
 
         <td className="px-4 py-4">
-          <div className="flex items-center space-x-4">
+          <div className={`flex items-center space-x-4 px-4`}>
             <DeleteModal
               onDelete={async () => {
                 // await alert({
@@ -116,43 +116,45 @@ export const MemberTableRow: React.FC<OrgTableRowType> = ({ data, key, loading }
               title="You are about to delete a member"
               subTitles={["This will delete your member forever", "Are you sure ?"]}
             />
-            <Popover className="">
-              {({ open }: any) => (
-                <>
-                  <Popover.Button>
-                    <MoreVertical
-                      name="more-vertical"
-                      className=" text-gray-400 cursor-pointer hover:text-gray-800 -ml-2 mt-1"
-                    />
-                  </Popover.Button>
+            {data.role_slug && data.role_id ? null : (
+              <Popover className="">
+                {({ open }: any) => (
+                  <>
+                    <Popover.Button>
+                      <MoreVertical
+                        name="more-vertical"
+                        className=" text-gray-400 cursor-pointer hover:text-gray-800 -ml-2 mt-1"
+                      />
+                    </Popover.Button>
 
-                  <Transition
-                    as={Fragment}
-                    enter="transition ease-out duration-200"
-                    enterFrom="opacity-0 -translate-y-1"
-                    enterTo="opacity-100 translate-y-0"
-                    leave="transition ease-in duration-150"
-                    leaveFrom="opacity-100 translate-y-0"
-                    leaveTo="opacity-0 -translate-y-1"
-                  >
-                    <Popover.Panel className="cursor-pointer absolute z-10 w-52 p-1 mt-3 right-14 bg-white ring-1 ring-black ring-opacity-5 rounded-sm shadow-lg space-y-2">
-                      <div
-                        className="overflow-hidden"
-                        onClick={() =>
-                          router.push(`/members/profile?id=${data.id}&role=${selectedRole.id}`)
-                        }
-                      >
-                        <a className="bg-white flex items-center transition duration-150 ease-in-out rounded-lg group hover:bg-green-600 focus:outline-none focus-visible:ring focus-visible:ring-green-500 focus-visible:ring-opacity-70">
-                          <div className="py-2 text-xl flex items-center px-4 gap-2 text-gray-700  group-hover:text-white ">
-                            <p className=" font-medium whitespace-nowrap  ">View Profile</p>
-                          </div>
-                        </a>
-                      </div>
-                    </Popover.Panel>
-                  </Transition>
-                </>
-              )}
-            </Popover>
+                    <Transition
+                      as={Fragment}
+                      enter="transition ease-out duration-200"
+                      enterFrom="opacity-0 -translate-y-1"
+                      enterTo="opacity-100 translate-y-0"
+                      leave="transition ease-in duration-150"
+                      leaveFrom="opacity-100 translate-y-0"
+                      leaveTo="opacity-0 -translate-y-1"
+                    >
+                      <Popover.Panel className="cursor-pointer absolute z-10 w-52 p-1 mt-3 right-14 bg-white ring-1 ring-black ring-opacity-5 rounded-sm shadow-lg space-y-2">
+                        <div
+                          className="overflow-hidden"
+                          onClick={() =>
+                            router.push(`/members/profile?id=${data.id}&role=${selectedRole.id}`)
+                          }
+                        >
+                          <a className="bg-white flex items-center transition duration-150 ease-in-out rounded-lg group hover:bg-green-600 focus:outline-none focus-visible:ring focus-visible:ring-green-500 focus-visible:ring-opacity-70">
+                            <div className="py-2 text-xl flex items-center px-4 gap-2 text-gray-700  group-hover:text-white ">
+                              <p className=" font-medium whitespace-nowrap  ">View Profile</p>
+                            </div>
+                          </a>
+                        </div>
+                      </Popover.Panel>
+                    </Transition>
+                  </>
+                )}
+              </Popover>
+            )}
           </div>
         </td>
       </tr>
