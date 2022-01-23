c49053858d53866c9bf9bd7cce3fd9459b801404
anup-stha <anup.stha012@gmail.com>
2022-01-23 22:09:42 +0545

    changes

--- src/pages/roles/index.tsx
+++ src/pages/roles/index.tsx
@@ -1,7 +1,7 @@
 /*
  * Created By Anup Shrestha
  * Copyright (c) 2021-2022. All rights reserved.
- * Last Modified 1/12/22, 1:59 PM
+ * Last Modified 1/23/22, 10:09 PM
  *
  *
  */
@@ -9,7 +9,6 @@
 import { RolePageLoadingState } from "@/components/state/rolePageLoadingState";
 import { withAuth } from "@/shared/hoc/withAuth";
 import { withRole } from "@/shared/hoc/withRole";
-import { MainLayout } from "@/layout/MainLayout";
 import RolePage from "@/modules/roles";
 import { useAllRoleList } from "@/modules/roles/hooks/useAllRoleList";
 import React from "react";
@@ -22,9 +21,7 @@ const Roles = () => {
     <>
       <MainHead title={`Roles`} />
 
-      <MainLayout>
-        {!isLoading ? <RolePage /> : <RolePageLoadingState count={1} />}
-      </MainLayout>
+      {!isLoading ? <RolePage /> : <RolePageLoadingState count={1} />}
     </>
   );
 };
