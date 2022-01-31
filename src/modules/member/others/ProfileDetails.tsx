825da9cfa6d3d43ae49bb6f70822bad9da4356d4
anup-stha <anup.stha012@gmail.com>
2022-01-31 13:28:12 +0545

    added cover image

--- src/modules/member/others/ProfileDetails.tsx
+++ src/modules/member/others/ProfileDetails.tsx
@@ -1,13 +1,14 @@
 /*
  * Created By Anup Shrestha
  * Copyright (c) 2021-2022. All rights reserved.
- * Last Modified 1/26/22, 8:22 PM
+ * Last Modified 1/31/22, 1:05 PM
  *
  *
  */
 
 import React from "react";
 import moment from "moment";
+import Image from "next/image";
 import { Member } from "@/modules/member/types";
 import { ProfilePrimaryDetails } from "@/modules/member/others/ProfilePrimaryDetails";
 import { ProfileOtherDetails } from "@/modules/member/others/ProfileOtherDetails";
@@ -23,7 +24,14 @@ export const ProfileDetails: React.FC<MemberDetailsProps> = ({
 }) => {
   return (
     <div className="print:hidden relative w-full bg-white rounded-xl sm:w-full ring-1 ring-black ring-opacity-10 overflow-hidden">
-      <div className="relative w-full h-52 z-0 profile" />
+      <div className="relative w-full h-52 z-0 profile">
+        <Image
+          src="/assets/cover-bg.svg"
+          layout={"fill"}
+          objectFit={"cover"}
+          objectPosition={"left center"}
+        />
+      </div>
 
       <ProfileHeader selectedMember={selectedMember} />
       {selectedMember && (
