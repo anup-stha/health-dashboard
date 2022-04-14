bf668939b36d673724068dd283482e590bef00a6
anup-stha <anup.stha012@gmail.com>
2022-04-14 09:38:55 +0545

    update

--- next.config.js
+++ next.config.js
@@ -1,9 +1,3 @@
-// This file sets a custom webpack configuration to use your Next.js app
-// with Sentry.
-// https://nextjs.org/docs/api-reference/next.config.js/introduction
-// https://docs.sentry.io/platforms/javascript/guides/nextjs/
-
-const { withSentryConfig } = require("@sentry/nextjs");
 const path = require("path");
 
 const moduleExports = {
@@ -27,6 +21,4 @@ const moduleExports = {
   },
 };
 
-// Make sure adding Sentry options is the last code to run before exporting, to
-// ensure that your source maps include changes from all other Webpack plugins
-module.exports = withSentryConfig(moduleExports);
+module.exports = moduleExports;
